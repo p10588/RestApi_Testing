51b246a1372e72efd4890b5b947cc94d7e0344c0
p10588 <p10588chain@gmail.com>
2023-09-10 23:32:00 +0800

    Update DiscordWebhookServer_Rest_Testing.py

--- DiscordWebhookServer_Rest_Testing.py
+++ DiscordWebhookServer_Rest_Testing.py
@@ -3,18 +3,15 @@ import requests
 
 # Discord Webhook URL
 webhook_url = os.getenv('DISCORD_URL')
-# 要发送的消息内容
+
 message = 'Hello, Discord! This is a test message Testing YAAAAAA.'
 
-# 构建消息数据
 data = {
     'content': message
 }
 
-# 发送 HTTP POST 请求到 Discord Webhook
 response = requests.post(webhook_url, json=data)
 
-# 检查响应状态码
 if response.status_code == 204:
     print('Message sent successfully!')
 else:
